31c6ab3dde85eeaaf2a26fe2bc1dc2a52ff091de
Ham Atila <hamatila7@gmail.com>
2021-11-14 16:23:33 +0200

    WebJobs

---
+++ HamAtilaGabriel/L05/Repository/StudentsRepository.cs
@@ -0,0 +1,48 @@
+﻿using L05.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace L05.Repository
+{
+    public class StudentsRepository:IStudentsRepository
+    {
+        private CloudTableClient _tableClient;
+        private CloudTable _studentsTable;
+        private string _connectionString;
+
+        private async Task InitializeTable()
+        {
+            var account = CloudStorageAccount.Parse(_connectionString);
+            _tableClient = account.CreateCloudTableClient();
+            _studentsTable = _tableClient.GetTableReference("studenti");
+            await _studentsTable.CreateIfNotExistsAsync();
+        }
+
+        public StudentsRepository()
+        {
+            _connectionString = "DefaultEndpointsProtocol=https;AccountName=hamatilal04;AccountKey=BXdnLDm6WcBHP9I3rrK5hl5+DdR4Lu3AUAKb2LJ/UdbR9VLXSWycJg5y8Fai/qhOAKjsuhlOYjL8Hzzsb+Uzcw==;EndpointSuffix=core.windows.net";
+            Task.Run(async () => { await InitializeTable(); })
+                .GetAwaiter()
+                .GetResult();
+        }
+
+        public async Task<List<StudentEntity>> GetAllStudents()
+        {
+            var students = new List<StudentEntity>();
+            TableQuery<StudentEntity> query = new TableQuery<StudentEntity>();
+            TableContinuationToken token = null;
+            do
+            {
+                TableQuerySegment<StudentEntity> resultSegment = await _studentsTable.ExecuteQuerySegmentedAsync(query, token);
+                token = resultSegment.ContinuationToken;
+                students.AddRange(resultSegment);
+            } while (token != null);
+            return students;
+        }
+    }
+}
